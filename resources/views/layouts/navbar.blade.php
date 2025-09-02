67ae368c4a5acd66e8477970a3533fb7dc108ac3
Zakaria-CPU301 <ramadanzaka27@gmail.com>
2025-09-02 20:02:35 +0700

    update rebase

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-gradient-to-r from-cyan-500 to-blue-300 px-8 py-5">
+<div class="bg-gradient-to-r from-[#1EB8CD] to-[#E7EDF5] px-8 py-5">
     <h1 class="text-white text-2xl font-bold">
         {{__('Hi')}} <span class="font-extrabold">Alejandro Garnacho!</span>
     </h1>
