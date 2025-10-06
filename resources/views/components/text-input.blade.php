e05f3607426891a9dfd3fe8e725657f0338bc997
asep <radikadybala10@gmail.com>
2025-10-06 13:37:46 +0700

    Fix text input

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(attributeDefaults: ['class' => 'border-black focus:border-indigo-500 rounded-md shadow-sm p-0']) }}>
+<input @disabled($disabled) {{ $attributes->merge(attributeDefaults: ['class' => 'border-black focus:border-indigo-500 rounded-md shadow-sm']) }}>
